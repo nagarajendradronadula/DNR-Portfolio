ba03bbc46d9836208e6e1ed5837ae6e7bd11fbc1
NAGA RAJENDRA DRONADULA <127020330+nagarajendradronadula@users.noreply.github.com>
2024-10-07 17:35:57 +0530

    Update home.jsx

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -11,7 +11,7 @@ const home = () => {
         <p className="text-sm md:text-2xl tracking-tight mt-5">
           MERN STACK DEVELOPER | JAVA | PYTHON
         </p>
-        <button className="mt-5 md:md-10 text-black p-y2 p-x3 text-sm md:text-lg md:p-y2 md:p-x4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg[#465697] border-solid bg-[#CAC9C7] p-3">
+        <button on click="#Footer" className="mt-5 md:md-10 text-black p-y2 p-x3 text-sm md:text-lg md:p-y2 md:p-x4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg[#465697] border-solid bg-[#CAC9C7] p-3">
           Contact Me!
         </button>
       </div>
